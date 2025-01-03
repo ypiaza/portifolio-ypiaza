35103e7b7c21e7ae956e1aa1536799dcb630c81f
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-01-03 13:24:19 -0300

    Criação da aba de projetos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,32 @@
-import React from 'react';
-import { Parallax } from 'react-parallax';
 import Me from '../assets/me.jpg';
 
 const About = () => {
   return (
-    <Parallax strength={500}>
-      <div className="h-screen grid grid-cols-2 items-center bg-gradient-to-r from-cyan-500 via-cyan-400 to-cyan-300">
-        {/* Seção da Imagem */}
-        <div
-          className="flex items-center justify-center"
-          data-aos="fade-left" // Animação AOS para saída pela esquerda
-        >
-          <img
-            className="w-[40%] rounded shadow-2xl border border-white/10"
-            src={Me}
-            alt="It's me!"
-          />
-        </div>
 
-        {/* Seção de Texto */}
-        <div
-          className="flex items-center justify-center rounded p-10 font-medium bg-gray-200 backdrop-blur border border-white/30 text-gray-900 shadow-2xl"
-          data-aos="fade-right" // Animação AOS para saída pela direita
-        >
-          <p>
-            Olá, eu sou Yuri Piaza, tenho 27 anos e sou estudante de Desenvolvimento Web, apaixonado por criar interfaces modernas e funcionais. Trabalho com HTML, CSS, JavaScript, Typescript, React e Tailwind CSS, focando em transformar ideias em experiências digitais atraentes, eficientes e responsivas. Estou sempre em busca de novos desafios para evoluir minhas habilidades em frontend e entregar soluções inovadoras, priorizando a experiência do usuário.
-          </p>
-        </div>
+    <div className="h-screen grid grid-cols-2 items-center bg-gradient-to-r from-cyan-800 via-cyan-700 to-cyan-300">
+      {/* Seção da Imagem */}
+      <div
+        className="flex items-center justify-center"
+        data-aos="fade-left" // Animação AOS para saída pela esquerda
+      >
+        <img
+          className="w-[40%] rounded shadow-2xl border border-white/10"
+          src={Me}
+          alt="It's me!"
+        />
       </div>
-    </Parallax>
+
+      {/* Seção de Texto */}
+      <div
+        className="flex items-center justify-center rounded p-10 font-medium bg-gray-200 backdrop-blur border border-white/30 text-gray-900 shadow-2xl"
+        data-aos="fade-right" // Animação AOS para saída pela direita
+      >
+        <p>
+          Olá, eu sou Yuri Piaza, tenho 27 anos e sou estudante de Desenvolvimento Web, apaixonado por criar interfaces modernas e funcionais. Trabalho com HTML, CSS, JavaScript, Typescript, React e Tailwind CSS, focando em transformar ideias em experiências digitais atraentes, eficientes e responsivas. Estou sempre em busca de novos desafios para evoluir minhas habilidades em frontend e entregar soluções inovadoras, priorizando a experiência do usuário.
+        </p>
+      </div>
+    </div>
+
   );
 };
 
---
+++ src/data/project.js
@@ -0,0 +1,8 @@
+export const projects = [
+    {name: 'To Do List', img: 'https://placehold.co/200x200', link: 'https://ypiaza.github.io/To-Do-List/', skills: 'Html, CSS, JavaScript'},
+    {name: 'EVA PROJECT TEST', img: 'https://placehold.co/200x200', link: 'https://ypiaza.github.io/EvA-projeto-test/', skills: 'React, Tailwind'},
+    {name: 'To Do List', img: 'https://placehold.co/200x200', link: 'https://ypiaza.github.io/To-Do-List/', skills: 'Html, CSS, JavaScript'},
+    {name: 'To Do List', img: 'https://placehold.co/200x200', link: 'https://ypiaza.github.io/To-Do-List/', skills: 'Html, CSS, JavaScript'},
+    {name: 'To Do List', img: 'https://placehold.co/200x200', link: 'https://ypiaza.github.io/To-Do-List/', skills: 'Html, CSS, JavaScript'},
+    {name: 'To Do List', img: 'https://placehold.co/200x200', link: 'https://ypiaza.github.io/To-Do-List/', skills: 'Html, CSS, JavaScript'},
+]
